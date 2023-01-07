a8ebb7abdeac37aba1c4e435b280f2dd02b38265
Andrea Burattin <andrea.burattin@gmail.com>
2023-01-07 23:33:10 +0100

    Moved requirements into setup file

--- setup.py
+++ setup.py
@@ -1,16 +1,21 @@
 from setuptools import find_packages, setup
 from os.path import dirname, join
 
+
 def read_file(filename):
     with open(join(dirname(__file__), filename)) as f:
         return f.read()
 
+
 setup(
     name="pybeamline",
     packages=find_packages(include=["pybeamline"]),
-    version="0.1.0",
-    description="Python version of Beamline (based on ReactiveX not Flink)",
+    version="0.0.2",
+    description="Python version of Beamline (based on ReactiveX)",
     author="Andrea Burattin",
     license="Apache-2.0",
-    install_requires=read_file("requirements.txt").split("\n"),
+    install_requires=[
+        'pm4py',
+        'reactivex'
+    ]
 )
