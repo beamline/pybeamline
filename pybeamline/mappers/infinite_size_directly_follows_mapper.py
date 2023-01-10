3a43f1d909a66ba24454a12adf8b7a74418adc23
Andrea Burattin <andrea.burattin@gmail.com>
2023-01-10 11:47:21 +0100

    Fixed proper typicig for tuples

--- pybeamline/mappers/infinite_size_directly_follows_mapper.py
+++ pybeamline/mappers/infinite_size_directly_follows_mapper.py
@@ -2,13 +2,13 @@ from reactivex import just, empty
 from reactivex import operators as ops
 from reactivex import Observable
 from pybeamline.bevent import BEvent
-from typing import Callable
+from typing import Callable, Tuple
 
 
-def infinite_size_directly_follows_mapper() -> Callable[[Observable[BEvent]], Observable[tuple[str, str]]]:
+def infinite_size_directly_follows_mapper() -> Callable[[Observable[BEvent]], Observable[Tuple[str, str]]]:
     map = {}
 
-    def extractor(event: BEvent) -> Observable[tuple[str, str]]:
+    def extractor(event: BEvent) -> Observable[Tuple[str, str]]:
         toRet = None
         if event.getTraceName() in map.keys():
             toRet = (map[event.getTraceName()], event.getEventName())
