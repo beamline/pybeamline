ff911190b471800f13c3cea66da2cfac949dc97d
Magnus-Frederiksen <Magnus.frederiksen86@gmail.com>
2023-06-20 11:29:33 +0200

    Bachelor-Project, Magnus F.

--- pybeamline/filters/__init__.py
+++
@@ -1,4 +0,0 @@
-from pybeamline.filters.filters import \
-    retains_activity_filter, excludes_activity_filter, \
-    retains_on_event_attribute_equal_filter, excludes_on_event_attribute_equal_filter, \
-    retains_on_trace_attribute_equal_filter, excludes_on_trace_attribute_equal_filter
