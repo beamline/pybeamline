f6a800f73ccd69d562de6ba257427ce2f12a5336
Andrea Burattin <andrea.burattin@gmail.com>
2026-01-16 17:01:04 +0100

    From format method to format string

--- pybeamline/mappers/print_operator.py
+++ pybeamline/mappers/print_operator.py
@@ -3,20 +3,20 @@ from typing_extensions import override
 
 from pybeamline.stream.base_map import BaseMap
 
-def print_operator(format_method: Callable = None) -> BaseMap[Any, Any]:
+def print_operator(format_method: str = None) -> BaseMap[Any, Any]:
     return PrintOperator(format_method=format_method)
 
 
 class PrintOperator(BaseMap[Any, Any]):
 
-    def __init__(self, format_method: Callable = None):
+    def __init__(self, format_method: str = None):
         self.format = format_method
 
 
     @override
     def transform(self, value: Any) -> Optional[List[Any]]:
         if self.format is not None:
-            print(self.format(value))
+            print(self.format.format(value))
         else:
             print(value)
         return [value]
